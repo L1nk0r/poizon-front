fd7a16b752e28f1b88a0615abf62ce8bd5b9f847
L1nk0r <1kozlov1bs41@gmail.com>
2023-03-10 21:50:42 +0300

    Vuex and axios in stack

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { createStore } from "vuex";
+import axios from "axios";
+
 import '../public/styles.css'
 
 createApp(App).mount('#app')
